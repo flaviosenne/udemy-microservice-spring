049984dc427a4e853dc4b7fa6e88b6f1503f6f96
joao flavio <flaviosenne123@gmail.com>
2022-01-10 14:07:03 -0300

    create class not found exception and implement methods CRUD in product

--- crud/src/main/java/com/microservice/crud/service/ProductService.java
+++ crud/src/main/java/com/microservice/crud/service/ProductService.java
@@ -1,9 +1,15 @@
 package com.microservice.crud.service;
 
 import com.microservice.crud.data.vo.ProductVO;
+import com.microservice.crud.entity.Product;
 import com.microservice.crud.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.lang.module.ResolutionException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +17,37 @@ public class ProductService {
     private final ProductRepository repository;
 
     public ProductVO create(ProductVO productVO){
-        return null;
+        return ProductVO.create(repository.save(ProductVO.create(productVO)));
+    }
+
+    public Page<ProductVO> findAll(Pageable pageable){
+        return repository.findAll(pageable).map(this::convertToProductVO);
+    }
+
+    public ProductVO findById(Long id){
+        return ProductVO.create(repository.findById(id).orElseThrow(()->
+                new ResolutionException("Produto não encontrado")));
+    }
+
+    public ProductVO update(ProductVO productVO){
+        final Optional<Product> existProduct = repository.findById(productVO.getId());
+
+        if(existProduct.isEmpty()){
+            throw new ResolutionException("Produto não encontrado");
+        }
+
+        return ProductVO.create(repository.save(ProductVO.create(productVO)));
+    }
+
+    public void delete(Long id){
+        Product entity = repository.findById(id).orElseThrow(() ->
+                new ResolutionException("Produto não encontrado"));
+
+        repository.delete(entity);
+    }
+
+
+    private ProductVO convertToProductVO(Product  product){
+        return ProductVO.create(product);
     }
 }
