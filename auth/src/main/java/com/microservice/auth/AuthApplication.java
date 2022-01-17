190d56009e6f20e49cd9add1ac0381ca23fbd45a
joao flavio <flaviosenne123@gmail.com>
2022-01-17 09:45:58 -0300

    register services in discovery application

--- auth/src/main/java/com/microservice/auth/AuthApplication.java
+++ auth/src/main/java/com/microservice/auth/AuthApplication.java
@@ -7,6 +7,7 @@ import com.microservice.auth.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -14,6 +15,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
